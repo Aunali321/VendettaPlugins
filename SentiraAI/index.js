b9436f6abfde11558f7c6d713e6f225e833f9663
Aunali321 <Aunali321@users.noreply.github.com>
2023-12-07 04:11:37 +0000

    deploy: 2ccb0301ba402c5ba0a434fc9d06718f7d4ddb08

--- SentiraAI/index.js
+++ SentiraAI/index.js
@@ -1,40 +1,4 @@
-(function(exports,_vendetta,commands$1,common,storage,plugin,components,assets){'use strict';var icons = {
-  Delete: assets.getAssetIDByName("ic_message_delete"),
-  Edit: assets.getAssetIDByName("ic_edit_24px")
-};const { FormRow, FormDivider, FormInput, FormSection } = components.Forms;
-function Settings() {
-  storage.useProxy(plugin.storage);
-  return /* @__PURE__ */ common.React.createElement(common.ReactNative.ScrollView, {
-    style: {
-      flex: 1
-    },
-    contentContainerStyle: {
-      paddingBottom: 38
-    }
-  }, /* @__PURE__ */ common.React.createElement(FormSection, {
-    title: "Authentication",
-    titleStyleType: "no_border"
-  }, /* @__PURE__ */ common.React.createElement(FormInput, {
-    title: "SentiraAI API Key",
-    leading: /* @__PURE__ */ common.React.createElement(FormRow.Icon, {
-      source: icons.Edit
-    }),
-    placeholder: "Enter your SentiraAI API Key",
-    value: plugin.storage.apiKey,
-    onChange: function(v) {
-      plugin.storage.apiKey = v;
-    }
-  }), /* @__PURE__ */ common.React.createElement(FormDivider, null), /* @__PURE__ */ common.React.createElement(FormRow, {
-    label: "Log out of SentiraAI",
-    subLabel: "This will clear your API token.",
-    leading: /* @__PURE__ */ common.React.createElement(FormRow.Icon, {
-      source: icons.Delete
-    }),
-    onPress: function() {
-      return plugin.storage.apiKey = "";
-    }
-  })));
-}function _class_call_check(instance, Constructor) {
+(function(exports,_vendetta,plugin,assets,common,components,storage,commands,toasts){'use strict';function _class_call_check(instance, Constructor) {
     if (!(instance instanceof Constructor)) throw new TypeError("Cannot call a class as a function");
 }function _defineProperties(target, props) {
     for(var i = 0; i < props.length; i++){
@@ -98,148 +62,250 @@ function _create_class(Constructor, protoProps, staticProps) {
     }
   ]);
   return SentiraAI2;
-}();var ApplicationCommandInputType;
-(function(ApplicationCommandInputType2) {
-  ApplicationCommandInputType2[ApplicationCommandInputType2["BUILT_IN"] = 0] = "BUILT_IN";
-  ApplicationCommandInputType2[ApplicationCommandInputType2["BUILT_IN_TEXT"] = 1] = "BUILT_IN_TEXT";
-  ApplicationCommandInputType2[ApplicationCommandInputType2["BUILT_IN_INTEGRATION"] = 2] = "BUILT_IN_INTEGRATION";
-  ApplicationCommandInputType2[ApplicationCommandInputType2["BOT"] = 3] = "BOT";
-  ApplicationCommandInputType2[ApplicationCommandInputType2["PLACEHOLDER"] = 4] = "PLACEHOLDER";
-})(ApplicationCommandInputType || (ApplicationCommandInputType = {}));
-var ApplicationCommandOptionType;
-(function(ApplicationCommandOptionType2) {
-  ApplicationCommandOptionType2[ApplicationCommandOptionType2["SUB_COMMAND"] = 1] = "SUB_COMMAND";
-  ApplicationCommandOptionType2[ApplicationCommandOptionType2["SUB_COMMAND_GROUP"] = 2] = "SUB_COMMAND_GROUP";
-  ApplicationCommandOptionType2[ApplicationCommandOptionType2["STRING"] = 3] = "STRING";
-  ApplicationCommandOptionType2[ApplicationCommandOptionType2["INTEGER"] = 4] = "INTEGER";
-  ApplicationCommandOptionType2[ApplicationCommandOptionType2["BOOLEAN"] = 5] = "BOOLEAN";
-  ApplicationCommandOptionType2[ApplicationCommandOptionType2["USER"] = 6] = "USER";
-  ApplicationCommandOptionType2[ApplicationCommandOptionType2["CHANNEL"] = 7] = "CHANNEL";
-  ApplicationCommandOptionType2[ApplicationCommandOptionType2["ROLE"] = 8] = "ROLE";
-  ApplicationCommandOptionType2[ApplicationCommandOptionType2["MENTIONABLE"] = 9] = "MENTIONABLE";
-  ApplicationCommandOptionType2[ApplicationCommandOptionType2["NUMBER"] = 10] = "NUMBER";
-  ApplicationCommandOptionType2[ApplicationCommandOptionType2["ATTACHMENT"] = 11] = "ATTACHMENT";
-})(ApplicationCommandOptionType || (ApplicationCommandOptionType = {}));
-var ApplicationCommandType;
-(function(ApplicationCommandType2) {
-  ApplicationCommandType2[ApplicationCommandType2["CHAT"] = 1] = "CHAT";
-  ApplicationCommandType2[ApplicationCommandType2["USER"] = 2] = "USER";
-  ApplicationCommandType2[ApplicationCommandType2["MESSAGE"] = 3] = "MESSAGE";
-})(ApplicationCommandType || (ApplicationCommandType = {}));let commands = [];
-const onLoad = function() {
-  _vendetta.logger.info("SentiraAI loaded");
-  _vendetta.logger.info("Registering commands");
-  commands.push(commands$1.registerCommand({
-    name: "summarize",
-    displayName: "Summarize",
-    description: "Summarize text",
-    displayDescription: "Summarize text",
-    applicationId: "-1",
-    inputType: 1,
-    type: 1,
-    options: [
-      {
-        name: "message",
-        displayName: "Message to summarize",
-        description: "Message to summarize",
-        displayDescription: "Message to summarize",
-        type: ApplicationCommandOptionType.STRING,
-        required: true
-      },
-      {
-        name: "length",
-        displayName: "Length of summary",
-        description: "Length of summary",
-        displayDescription: "Length of summary",
-        type: ApplicationCommandOptionType.STRING,
-        required: false,
-        // @ts-ignore
-        choices: [
-          {
-            name: "Short",
-            value: "short"
-          },
-          {
-            name: "Medium",
-            value: "medium"
-          },
-          {
-            name: "Long",
-            value: "long"
-          }
-        ]
-      },
-      {
-        name: "format",
-        displayName: "Format of summary",
-        description: "Format of summary",
-        displayDescription: "Format of summary",
-        type: ApplicationCommandOptionType.STRING,
-        required: false,
-        // @ts-ignore
-        choices: [
-          {
-            name: "Paragraph",
-            value: "paragraph"
-          },
-          {
-            name: "Bullets",
-            value: "bullets"
-          }
-        ]
-      },
-      {
-        name: "model",
-        displayName: "Model to use",
-        description: "Model to use",
-        displayDescription: "Model to use",
-        type: ApplicationCommandOptionType.STRING,
-        required: true,
-        // @ts-ignore
-        choices: [
-          {
-            name: "Fast",
-            value: "command-light"
-          },
-          {
-            name: "Slow",
-            value: "command"
-          },
-          {
-            name: "Fast Nightly",
-            value: "command-light-nightly"
-          },
-          {
-            name: "Slow Nightly",
-            value: "command-nightly"
-          }
-        ]
-      }
-    ],
-    execute: async function(args, ctx) {
-      _vendetta.logger.info("SentiraAI summarize command executed");
-      const message = args.message;
-      const length = args.length;
-      const format = args.format;
-      const model = args.model;
-      const sentiraAI = new SentiraAI("http://sentiraai.auna.li", _vendetta.storage.apiKey || "54321");
-      sentiraAI.summarize({
-        userId: "123",
-        text: message,
-        summaryLength: length,
-        summaryFormat: format,
-        model: model || "command-light"
-      }).then(function(response) {
-        console.log(response);
-      }).catch(function(error) {
-        console.error(error);
-      });
+}();var icons = {
+  Delete: assets.getAssetIDByName("ic_message_delete"),
+  Edit: assets.getAssetIDByName("ic_edit_24px")
+};const FriendlyLengthNames = {
+  auto: "Auto",
+  short: "Short",
+  medium: "Medium",
+  long: "Long"
+};
+const FriendlyFormatNames = {
+  paragraph: "Paragraph",
+  bullets: "Bullets"
+};
+const FriendlyModelNames = {
+  command: "Slow",
+  "command-light": "Fast",
+  "command-nightly": "Slow (Nightly)",
+  "command-light-nightly": "Fast (Nightly)"
+};const { FormRow: FormRow$3 } = components.Forms;
+const { ScrollView: ScrollView$3 } = common.ReactNative;
+const models = [
+  "command",
+  "command-light",
+  "command-nightly",
+  "command-light-nightly"
+];
+function Model() {
+  storage.useProxy(settings);
+  return /* @__PURE__ */ common.React.createElement(ScrollView$3, {
+    style: {
+      flex: 1
     }
+  }, models.map(function(model) {
+    return /* @__PURE__ */ common.React.createElement(FormRow$3, {
+      label: FriendlyModelNames[model],
+      trailing: function() {
+        return /* @__PURE__ */ common.React.createElement(FormRow$3.Arrow, null);
+      },
+      onPress: function() {
+        if (settings.model === model)
+          return;
+        settings.model = model;
+        toasts.showToast(`Saved model to ${FriendlyModelNames[model]}`, assets.getAssetIDByName("check"));
+      }
+    });
   }));
-};
-const onUnload = function() {
-  _vendetta.logger.info("SentiraAI unloaded");
-  for (const unregisterCommands of commands)
-    unregisterCommands();
-};
-const settings = Settings;exports.onLoad=onLoad;exports.onUnload=onUnload;exports.settings=settings;return exports;})({},vendetta,vendetta.commands,vendetta.metro.common,vendetta.storage,vendetta.plugin,vendetta.ui.components,vendetta.ui.assets);
+}const { FormRow: FormRow$2 } = components.Forms;
+const { ScrollView: ScrollView$2 } = common.ReactNative;
+const formats = [
+  "paragraph",
+  "bullets"
+];
+function Format() {
+  storage.useProxy(settings);
+  return /* @__PURE__ */ common.React.createElement(ScrollView$2, {
+    style: {
+      flex: 1
+    }
+  }, formats.map(function(format) {
+    return /* @__PURE__ */ common.React.createElement(FormRow$2, {
+      label: FriendlyFormatNames[format],
+      trailing: function() {
+        return /* @__PURE__ */ common.React.createElement(FormRow$2.Arrow, null);
+      },
+      onPress: function() {
+        if (settings.format === format)
+          return;
+        settings.format = format;
+        toasts.showToast(`Saved model to ${FriendlyFormatNames[format]}`, assets.getAssetIDByName("check"));
+      }
+    });
+  }));
+}const { FormRow: FormRow$1 } = components.Forms;
+const { ScrollView: ScrollView$1 } = common.ReactNative;
+const lengths = [
+  "auto",
+  "short",
+  "medium",
+  "long"
+];
+function DefaultLength() {
+  storage.useProxy(settings);
+  return /* @__PURE__ */ common.React.createElement(ScrollView$1, {
+    style: {
+      flex: 1
+    }
+  }, lengths.map(function(length) {
+    return /* @__PURE__ */ common.React.createElement(FormRow$1, {
+      label: FriendlyLengthNames[length],
+      trailing: function() {
+        return /* @__PURE__ */ common.React.createElement(FormRow$1.Arrow, null);
+      },
+      onPress: function() {
+        if (settings.defaultLength == length)
+          return;
+        settings.defaultLength = length;
+        toasts.showToast(`Saved format to ${FriendlyLengthNames[length]}`, assets.getAssetIDByName("check"));
+      }
+    });
+  }));
+}const { FormRow, FormDivider, FormInput, FormSection } = components.Forms;
+const { ScrollView } = common.ReactNative;
+function Settings() {
+  const navigation = common.NavigationNative.useNavigation();
+  storage.useProxy(settings);
+  const categories = [
+    {
+      label: "Model to use",
+      sublabel: FriendlyModelNames[settings.model],
+      icon: "ic_activity_24px",
+      render: Model,
+      renderTitle: "Model"
+    },
+    {
+      label: "Format of summary",
+      sublabel: FriendlyFormatNames[settings.format],
+      icon: "ic_activity_24px",
+      render: Format,
+      renderTitle: "Format"
+    },
+    {
+      label: "Default length of summary",
+      sublabel: FriendlyLengthNames[settings.defaultLength],
+      icon: "ic_activity_24px",
+      render: DefaultLength,
+      renderTitle: "DefaultLength"
+    }
+  ];
+  return /* @__PURE__ */ common.React.createElement(ScrollView, null, /* @__PURE__ */ common.React.createElement(FormSection, {
+    title: "Authentication",
+    titleStyleType: "no_border"
+  }, /* @__PURE__ */ common.React.createElement(FormInput, {
+    title: "SentiraAI API Key",
+    leading: /* @__PURE__ */ common.React.createElement(FormRow.Icon, {
+      source: icons.Edit
+    }),
+    placeholder: "Enter your SentiraAI API Key",
+    value: settings.apiKey,
+    onChange: function(v) {
+      settings.apiKey = v;
+    }
+  }), /* @__PURE__ */ common.React.createElement(FormDivider, null), /* @__PURE__ */ common.React.createElement(FormRow, {
+    label: "Log out of SentiraAI",
+    subLabel: "This will clear your API token.",
+    leading: /* @__PURE__ */ common.React.createElement(FormRow.Icon, {
+      source: icons.Delete
+    }),
+    onPress: function() {
+      return settings.apiKey = "";
+    }
+  })), categories.map(function(param) {
+    let { label, sublabel, icon, render } = param;
+    return /* @__PURE__ */ common.React.createElement(FormRow, {
+      label,
+      subLabel: sublabel,
+      leading: /* @__PURE__ */ common.React.createElement(FormRow.Icon, {
+        source: assets.getAssetIDByName(icon)
+      }),
+      trailing: function() {
+        return /* @__PURE__ */ common.React.createElement(FormRow.Arrow, null);
+      },
+      onPress: function() {
+        return navigation.push("VendettaCustomPage", {
+          title: "Model",
+          render
+        });
+      }
+    });
+  }));
+}let patches = [];
+const settings = plugin.storage;
+settings.model = "command-light";
+settings.format = "paragraph";
+settings.defaultLength = "auto";
+const lengthChoices = [
+  "auto",
+  "short",
+  "medium",
+  "long"
+];
+var index = {
+  onLoad: function() {
+    _vendetta.logger.info("SentiraAI loaded");
+    _vendetta.logger.info("Registering commands");
+    patches.push(commands.registerCommand({
+      name: "summarize",
+      displayName: "Summarize",
+      description: "Summarize text",
+      displayDescription: "Summarize text",
+      options: [
+        {
+          name: "message",
+          displayName: "message",
+          description: "Message",
+          displayDescription: "Message",
+          required: true,
+          type: 3
+        },
+        {
+          name: "length",
+          displayName: "length",
+          description: "Length of summary",
+          displayDescription: "Length of summary",
+          required: true,
+          type: 3,
+          // @ts-ignore
+          choices: lengthChoices.map(function(length) {
+            return {
+              name: FriendlyLengthNames[length],
+              displayName: FriendlyLengthNames[length],
+              value: length
+            };
+          })
+        }
+      ],
+      // @ts-ignore
+      applicationId: -1,
+      inputType: 1,
+      type: 1,
+      execute: async function(args) {
+        _vendetta.logger.info("SentiraAI summarize command executed");
+        const message = args[0]?.message;
+        const length = args[0]?.length;
+        const format = settings.format;
+        const model = settings.model;
+        const sentiraAI = new SentiraAI("http://sentiraai.auna.li", plugin.storage.apiKey || "54321");
+        sentiraAI.summarize({
+          userId: "123",
+          text: message,
+          summaryLength: length,
+          summaryFormat: format,
+          model
+        }).then(function(response) {
+          console.log(response);
+        }).catch(function(error) {
+          console.error(error);
+        });
+      }
+    }));
+  },
+  onUnload: function() {
+    _vendetta.logger.info("SentiraAI unloaded");
+    for (const unpatch of patches)
+      unpatch();
+  },
+  settings: Settings
+};exports.default=index;exports.settings=settings;Object.defineProperty(exports,'__esModule',{value:true});return exports;})({},vendetta,vendetta.plugin,vendetta.ui.assets,vendetta.metro.common,vendetta.ui.components,vendetta.storage,vendetta.commands,vendetta.ui.toasts);
